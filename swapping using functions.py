be14f6f20a347eba461966ffb3360e3743b89494
98nikhildaphale <71168545+98nikhildaphale@users.noreply.github.com>
2021-07-02 23:27:06 +0530

    Add files via upload

---
+++ swapping using functions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 22:43:17 2021
+
+@author: trupti
+"""
+
+
+def swap():
+    x=int(input())
+    y=int(input())
+    x,y=y,x
+    print("after swapping x:",x,"and y:",y)
+    
+swap()    
+    
